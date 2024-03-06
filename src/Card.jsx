b30fe65a6910903150f7ee5eee40c65e17e3b5d2
Taha <60511036+tahamandviwala4@users.noreply.github.com>
2024-03-06 17:30:08 +0530

    fixing image

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 import Paragraph from "./Paragraph";
 import './card.css';
+import imgURL from '../public/image-qr-code.png';
 
 export default function Card(){
     return(
     <div className='card'>
         <div className='container'>
-            <img src="../public/image-qr-code.png" alt="qr code image" className='image'/>
+            <img src={imgURL} alt="qr code image" className='image'/>
             <Paragraph ></Paragraph>
         </div>
     </div>
